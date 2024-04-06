0246210e59c2c0eea2b095ccff76d01efd4a26fe
atharvsoni2004 <128619435+atharvsoni2004@users.noreply.github.com>
2024-04-06 19:30:31 +0530

    mistake

--- src/Menu.js
+++ src/Menu.js
@@ -1,11 +1,79 @@
 import { Link } from "react-router-dom";
+import {useCookies} from "react-cookie";
 function showhideSidebar() {
      //   alert('hi');
      document.body.classList.toggle('toggle-sidebar');
 }
+function Adminmenuitems ()
+{ 
+     let [cookies,setCookies,removeCookies]=useCookies(['Atharv']);
+      if(cookies['adminid']!==undefined)
+      {
+               <>< li className="nav-item shadow-sm" >
+                    <Link className="nav-link collapsed" to="/admin-home">
+                         <i className="fa fa-home fa-2x" />
+                         <span>Home</span>
+                    </Link>
+               </li >
+                    <li className="nav-item shadow-sm">
+                         <Link className="nav-link collapsed" to="/admin-doctor-management">
+                              <i className="fa fa-home fa-2x" />
+                              <span>Doctors Management</span>
+                         </Link>
+          </li></>;
+     }
+}
+function Doctormenuitems(){    
+     let [cookies,setCookies,removeCookies]=useCookies(['Atharv'])
+     if(cookies['doctorid']!==undefined)
+     {  return(<>
+               <li className="nav-item shadow-sm">
+                    <Link className="nav-link collapsed" to="/doctor-home">
+                         <i className="fa fa-home fa-2x" />
+                         <span>Home</span>
+                    </Link>
+               </li>
+               <li className="nav-item shadow-sm">
+                    <Link className="nav-link collapsed" to="/doctor-profile">
+                         <i className="fa fa-home fa-2x" />
+                         <span>My Profile</span>
+                    </Link>
+               </li>
+               <li className="nav-item shadow-sm">
+                              <Link className="nav-link collapsed" to="/admin-package">
+                         <i className="fa fa-home fa-2x" />
+                         <span>My Package</span>
+                    </Link>
+               </li>
+               <li className="nav-item shadow-sm">
+                              <Link className="nav-link collapsed" to="/admin-assitant">
+                         <i className="fa fa-home fa-2x" />
+                         <span>My Assitants</span>
+                    </Link>
+               </li>
+               <li className="nav-item shadow-sm">
+                              <Link className="nav-link collapsed" to="/admin-appointment">
+                         <i className="fa fa-home fa-2x" />
+                         <span>My Appointments</span>
+                    </Link>
+                         </li> </>)
+     }
+}
+function Assitantsmenuitems()
+{    
+     let [cookies,setCookies,removeCookies]=useCookies(['Atharv']);
+     if(cookies['assistantid']!==undefined)
+     return(<>
+               <li className="nav-item shadow-sm">
+                    <Link className="nav-link collapsed" to="/admin-appointment">
+                         <i className="fa fa-home fa-2x" />
+                         <span>Home</span>
+                    </Link>
+               </li>
+          </>)
+}
 export default function Menu() {
-     return ( //j
-          <>
+return (    <>
                <header id="header" className="header fixed-top d-flex align-items-center">
                     <div className="d-flex align-items-center justify-content-between">
                          <Link to="/admin-home" className="logo d-flex align-items-center">
@@ -28,58 +96,9 @@ export default function Menu() {
                {/* sidebar */}
                <aside id="sidebar" className="sidebar">
                     <ul className="sidebar-nav" id="sidebar-nav">
-                         {/* links for admin  */}
-
-                         <li className="nav-item shadow-sm">
-                              <Link className="nav-link collapsed" to="/admin-home">
-                                   <i className="fa fa-home fa-2x" />
-                                   <span>Home</span>
-                              </Link>
-                         </li>
-                         <li className="nav-item shadow-sm">
-                              <Link className="nav-link collapsed" to="/admin-doctor-management">
-                                   <i className="fa fa-home fa-2x" />
-                                   <span>Doctors Management</span>
-                              </Link>
-                         </li>
-                         {/* links for doctor  */}
-                         <li className="nav-item shadow-sm">
-                              <Link className="nav-link collapsed" to="/doctor-home">
-                                   <i className="fa fa-home fa-2x" />
-                                   <span>Home</span>
-                              </Link>
-                         </li>
-                         <li className="nav-item shadow-sm">
-                              <Link className="nav-link collapsed" to="/doctor-profile">
-                                   <i className="fa fa-home fa-2x" />
-                                   <span>My Profile</span>
-                              </Link>
-                         </li>
-                         <li className="nav-item shadow-sm">
-                              <Link className="nav-link collapsed" to="/admin-package">
-                                   <i className="fa fa-home fa-2x" />
-                                   <span>My Package</span>
-                              </Link>
-                         </li>
-                         <li className="nav-item shadow-sm">
-                              <Link className="nav-link collapsed" to="/admin-assitant">
-                                   <i className="fa fa-home fa-2x" />
-                                   <span>My Assitants</span>
-                              </Link>
-                         </li>
-                         <li className="nav-item shadow-sm">
-                              <Link className="nav-link collapsed" to="/admin-appointment">
-                                   <i className="fa fa-home fa-2x" />
-                                   <span>My Appointments</span>
-                              </Link>
-                         </li>
-                         {/* links for assitant */}
-                         <li className="nav-item shadow-sm">
-                              <Link className="nav-link collapsed" to="/admin-appointment">
-                                   <i className="fa fa-home fa-2x" />
-                                   <span>Home</span>
-                              </Link>
-                         </li>
+                        <Adminmenuitems/>
+                        <Doctormenuitems/>
+                        <Assitantsmenuitems/>
                          {/* links for all users (admin/doctor/assitant) */}
                          <li className="nav-item shadow-sm">
                               <Link className="nav-link collapsed" to="/admin-change-password">
